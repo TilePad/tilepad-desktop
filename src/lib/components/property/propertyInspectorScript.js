45f32e1011da7ebf16e0663656a5162ad86e309e
Jacobtread <jacobtread@gmail.com>
2025-03-02 01:13:49 +1300

    feat: inspector debounce helper

--- src/lib/components/property/propertyInspectorScript.js
+++ src/lib/components/property/propertyInspectorScript.js
@@ -8,3 +8,25 @@ channel.onmessage = (event) => {
 };
 
 channel.postMessage("Hello from Iframe");
+
+function debounce(fn, delay) {
+  let timeoutId;
+
+  return function (...args) {
+    // Clear the previous timeout
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    // Set a new timeout with the specified delay
+    timeoutId = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+function setSetting(name, value) {}
+
+window.tilepad = {
+  setSetting: debounce(setSetting, 100),
+};
