a57474f01120081f4d8bfe10c8dc030521a79824
Jacobtread <jacobtread@gmail.com>
2025-03-07 22:49:29 +1300

    fix: reset active tile when switching folder or profile

--- src/lib/components/folders/FolderEditor.svelte
+++ src/lib/components/folders/FolderEditor.svelte
@@ -1,6 +1,8 @@
+<!-- Editor for editing the contents of a folder -->
 <script lang="ts">
   import type { TileId, TileModel } from "$lib/api/types/tiles";
 
+  import { watch } from "runed";
   import { createTilesQuery } from "$lib/api/tiles";
   import { getErrorMessage } from "$lib/api/utils/error";
   import TileGrid from "$lib/components/tiles/TileGrid.svelte";
@@ -9,17 +11,32 @@
   import FilledTile from "../tiles/FilledTile.svelte";
   import TileEditor from "../tiles/TileEditor.svelte";
   import { getFolderContext } from "./FolderProvider.svelte";
+  import { getProfileContext } from "../profiles/ProfilesProvider.svelte";
 
+  const { profile } = getProfileContext();
   const { folder } = getFolderContext();
-  const currentFolder = $derived.by(folder);
 
-  const tilesQuery = createTilesQuery(() => currentFolder.id);
+  const currentFolder = $derived.by(folder);
+  const currentProfile = $derived.by(profile);
+
+  const currentFolderId = $derived(currentFolder.id);
+  const currentProfileId = $derived(currentProfile.id);
+
+  const tilesQuery = createTilesQuery(() => currentFolderId);
 
   let activeTileId: TileId | null = $state(null);
 
   function getTile(tiles: TileModel[], row: number, column: number) {
     return tiles.find((tile) => tile.row === row && tile.column === column);
   }
+
+  // Clear the active tile whenever switching profile or folders
+  watch(
+    () => ({ currentFolderId, currentProfileId }),
+    () => {
+      activeTileId = null;
+    },
+  );
 </script>
 
 {#if $tilesQuery.isLoading}
@@ -53,6 +70,7 @@
       </TileGrid>
     </div>
 
+    <!-- Bottom segment that pops up to edit a tile -->
     {#if activeTileId !== null}
       <TileEditor tileId={activeTileId} onClose={() => (activeTileId = null)} />
     {/if}
