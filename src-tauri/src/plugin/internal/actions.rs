12a62f22b9feb8161327a19e459c4173e50c7b39
Jacobtread <jacobtread@gmail.com>
2025-04-03 21:49:18 +1300

    feat: hook up switch profile internal action

--- src-tauri/src/plugin/internal/actions.rs
+++ src-tauri/src/plugin/internal/actions.rs
@@ -6,6 +6,7 @@ use crate::{
         entity::{
             device::{DeviceModel, UpdateDevice},
             folder::{FolderId, FolderModel},
+            profile::{ProfileId, ProfileModel},
             tile::TileModel,
         },
         DbPool,
@@ -40,6 +41,11 @@ pub struct SwitchFolderProperties {
     folder: FolderId,
 }
 
+#[derive(Deserialize)]
+pub struct SwitchProfileProperties {
+    profile: ProfileId,
+}
+
 async fn handle_internal_navigation(
     devices: &Devices,
     plugins: &Plugins,
@@ -65,6 +71,36 @@ async fn handle_internal_navigation(
                     db,
                     UpdateDevice {
                         folder_id: Some(folder_id),
+                        ..Default::default()
+                    },
+                )
+                .await?;
+
+            if let Some(session) = devices.get_session_by_device(context.device_id) {
+                session.send_message(ServerDeviceMessage::Tiles { tiles, folder })?;
+            }
+        }
+        "switch_profile" => {
+            let device = DeviceModel::get_by_id(db, context.device_id)
+                .await?
+                .context("device not found")?;
+            let data: SwitchProfileProperties = serde_json::from_value(tile.config.properties)?;
+
+            let profile_id = data.profile;
+            let profile = ProfileModel::get_by_id(db, profile_id)
+                .await?
+                .context("unknown profile")?;
+            let folder = FolderModel::get_default(db, profile_id)
+                .await?
+                .context("unknown folder")?;
+            let tiles = TileModel::get_by_folder(db, folder.id).await?;
+
+            device
+                .update(
+                    db,
+                    UpdateDevice {
+                        profile_id: Some(profile_id),
+                        folder_id: Some(folder.id),
                         ..Default::default()
                     },
                 )
--- src-tauri/src/plugin/internal/messages.rs
+++ src-tauri/src/plugin/internal/messages.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     database::{
-        entity::{folder::FolderModel, tile::TileModel},
+        entity::{folder::FolderModel, profile::ProfileModel, tile::TileModel},
         DbPool,
     },
     events::{AppEvent, AppEventSender, InspectorContext, PluginAppEvent},
@@ -38,17 +38,19 @@ pub async fn handle_internal_send_message(
 #[derive(Deserialize)]
 #[serde(tag = "type", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum NavigationInspectorMessage {
-    GetFolderOptions {},
+    GetFolderOptions,
+    GetProfileOptions,
 }
 
 #[derive(Serialize)]
 #[serde(tag = "type", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum NavigationPluginMessage {
-    FolderOptions { options: Vec<FolderOption> },
+    FolderOptions { options: Vec<SelectOption> },
+    ProfileOptions { options: Vec<SelectOption> },
 }
 
 #[derive(Serialize)]
-pub struct FolderOption {
+pub struct SelectOption {
     pub label: String,
     pub value: String,
 }
@@ -65,17 +67,34 @@ async fn handle_internal_navigation(
     let message: NavigationInspectorMessage = serde_json::from_value(message)?;
 
     match message {
-        NavigationInspectorMessage::GetFolderOptions {} => {
+        NavigationInspectorMessage::GetFolderOptions => {
             let folders = FolderModel::all(db, context.profile_id).await?;
             let options = folders
                 .into_iter()
-                .map(|folder| FolderOption {
+                .map(|folder| SelectOption {
                     label: folder.name,
                     value: folder.id.to_string(),
                 })
                 .collect();
 
             let message = NavigationPluginMessage::FolderOptions { options };
+            let message = serde_json::to_value(message)?;
+            app_tx.send(AppEvent::Plugin(PluginAppEvent::RecvPluginMessage {
+                context,
+                message,
+            }))?;
+        }
+        NavigationInspectorMessage::GetProfileOptions => {
+            let profiles = ProfileModel::all(db).await?;
+            let options = profiles
+                .into_iter()
+                .map(|profile| SelectOption {
+                    label: profile.name,
+                    value: profile.id.to_string(),
+                })
+                .collect();
+
+            let message = NavigationPluginMessage::ProfileOptions { options };
             let message = serde_json::to_value(message)?;
             app_tx.send(AppEvent::Plugin(PluginAppEvent::RecvPluginMessage {
                 context,
