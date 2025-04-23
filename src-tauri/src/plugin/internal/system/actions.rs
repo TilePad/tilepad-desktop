bed855040aa3a7230e089225c3e16aa1e501d6fd
Jacobtread <jacobtread@gmail.com>
2025-04-23 22:14:42 +1200

    fix: move enigo actions to separate thread to support multiple platforms

--- src-tauri/src/plugin/internal/system/actions.rs
+++ src-tauri/src/plugin/internal/system/actions.rs
@@ -1,11 +1,9 @@
-use std::{path::Path, time::Duration};
-
 use arboard::Clipboard;
 use enigo::{Enigo, Key, Keyboard};
 use serde::Deserialize;
+use std::{path::Path, time::Duration};
 use sysinfo::{ProcessRefreshKind, ProcessesToUpdate, RefreshKind, System, UpdateKind};
 use tauri_plugin_opener::{open_path, open_url};
-use tokio::time::sleep;
 
 use crate::{database::JsonObject, events::TileInteractionContext};
 
@@ -118,8 +116,114 @@ pub async fn handle(context: TileInteractionContext, properties: JsonObject) -> 
             let data: SystemTextProperties =
                 serde_json::from_value(serde_json::Value::Object(properties))?;
             if let Some(text) = data.text {
-                let mut enigo = Enigo::new(&enigo::Settings::default())?;
+                execute_enigo_action(EnoAction::Text { text });
+            }
+        }
+        "multimedia" => {
+            let data: SystemMultimediaProperties =
+                serde_json::from_value(serde_json::Value::Object(properties))?;
 
+            let action = match data.action {
+                Some(value) => value,
+                None => return Ok(()),
+            };
+
+            let key: Key = match action {
+                MultimediaAction::PlayPause => Key::MediaPlayPause,
+                // Actual play button only supported on windows
+                #[cfg(windows)]
+                MultimediaAction::Play => Key::Play,
+                // Non windows targets fall back to the play pause key
+                #[cfg(not(windows))]
+                MultimediaAction::Play => Key::MediaPlayPause,
+
+                // Pause button only supported on windows and linux
+                #[cfg(any(target_os = "windows", all(unix, not(target_os = "macos"))))]
+                MultimediaAction::Pause => Key::Pause,
+
+                // Mac falls back to the play pause toggle
+                #[cfg(target_os = "macos")]
+                MultimediaAction::Pause => Key::MediaPlayPause,
+
+                MultimediaAction::NextTrack => Key::MediaNextTrack,
+                MultimediaAction::PreviousTrack => Key::MediaPrevTrack,
+                MultimediaAction::VolumeUp => Key::VolumeUp,
+                MultimediaAction::VolumeDown => Key::VolumeDown,
+                MultimediaAction::Mute => Key::VolumeMute,
+            };
+
+            execute_enigo_action(EnoAction::Key { key });
+        }
+        "hotkey" => {
+            let data: HotkeyProperties =
+                serde_json::from_value(serde_json::Value::Object(properties))?;
+
+            let keys = match data.keys {
+                Some(value) => value,
+                None => return Ok(()),
+            };
+
+            let modifiers: Vec<Key> = keys
+                .modifiers
+                .into_iter()
+                .map(|key| Key::Other(key.code))
+                .collect();
+
+            let keys: Vec<Key> = keys
+                .keys
+                .into_iter()
+                .map(|key| Key::Other(key.code))
+                .collect();
+
+            execute_enigo_action(EnoAction::HotKey { modifiers, keys });
+        }
+        "clipboard" => {
+            let data: ClipboardProperties =
+                serde_json::from_value(serde_json::Value::Object(properties))?;
+            if let Some(text) = data.text {
+                let mut clipboard = Clipboard::new()?;
+                clipboard.set_text(text)?;
+            }
+        }
+        action_id => {
+            tracing::warn!(?action_id, ?context, "unknown internal action");
+        }
+    }
+
+    Ok(())
+}
+
+pub enum EnoAction {
+    HotKey { modifiers: Vec<Key>, keys: Vec<Key> },
+    Key { key: Key },
+    Text { text: String },
+}
+
+/// Executes an enigo action, on windows these can be done in an async environment
+/// but on other platforms these are !Send so have to be performed in a dedicated
+/// thread
+fn execute_enigo_action(action: EnoAction) {
+    std::thread::spawn(|| {
+        let mut enigo = Enigo::new(&enigo::Settings::default())?;
+
+        match action {
+            EnoAction::HotKey { modifiers, keys } => {
+                for key in &modifiers {
+                    enigo.key(*key, enigo::Direction::Press)?;
+                }
+
+                for key in keys {
+                    enigo.key(key, enigo::Direction::Click)?;
+                }
+
+                for key in modifiers {
+                    enigo.key(key, enigo::Direction::Release)?;
+                }
+            }
+            EnoAction::Key { key } => {
+                enigo.key(key, enigo::Direction::Click)?;
+            }
+            EnoAction::Text { text } => {
                 let mut current_text = String::new();
 
                 for char in text.chars() {
@@ -146,98 +250,11 @@ pub async fn handle(context: TileInteractionContext, properties: JsonObject) -> 
                     }
 
                     // Sleep between sends
-                    sleep(Duration::from_millis(2)).await;
+                    std::thread::sleep(Duration::from_millis(2));
                 }
             }
         }
-        "multimedia" => {
-            let data: SystemMultimediaProperties =
-                serde_json::from_value(serde_json::Value::Object(properties))?;
 
-            let action = match data.action {
-                Some(value) => value,
-                None => return Ok(()),
-            };
-
-            let mut enigo = Enigo::new(&enigo::Settings::default())?;
-
-            match action {
-                MultimediaAction::PlayPause => {
-                    enigo.key(Key::MediaPlayPause, enigo::Direction::Click)?;
-                }
-                MultimediaAction::Play => {
-                    // Actual play button only supported on windows
-                    #[cfg(windows)]
-                    enigo.key(Key::Play, enigo::Direction::Click)?;
-
-                    // Non windows targets fall back to the play pause key
-                    #[cfg(not(windows))]
-                    enigo.key(Key::MediaPlayPause, enigo::Direction::Click)?;
-                }
-                MultimediaAction::Pause => {
-                    // Pause button only supported on windows and linux
-                    #[cfg(any(target_os = "windows", all(unix, not(target_os = "macos"))))]
-                    enigo.key(Key::Pause, enigo::Direction::Click)?;
-
-                    // Mac falls back to the play pause toggle
-                    #[cfg(target_os = "macos")]
-                    enigo.key(Key::MediaPlayPause, enigo::Direction::Click)?;
-                }
-                MultimediaAction::NextTrack => {
-                    enigo.key(Key::MediaNextTrack, enigo::Direction::Click)?;
-                }
-                MultimediaAction::PreviousTrack => {
-                    enigo.key(Key::MediaPrevTrack, enigo::Direction::Click)?;
-                }
-                MultimediaAction::VolumeUp => {
-                    enigo.key(Key::VolumeUp, enigo::Direction::Click)?;
-                }
-                MultimediaAction::VolumeDown => {
-                    enigo.key(Key::VolumeDown, enigo::Direction::Click)?;
-                }
-                MultimediaAction::Mute => {
-                    enigo.key(Key::VolumeMute, enigo::Direction::Click)?;
-                }
-            }
-        }
-        "hotkey" => {
-            let data: HotkeyProperties =
-                serde_json::from_value(serde_json::Value::Object(properties))?;
-
-            let keys = match data.keys {
-                Some(value) => value,
-                None => return Ok(()),
-            };
-
-            let mut enigo = Enigo::new(&enigo::Settings::default())?;
-
-            for key in &keys.modifiers {
-                let key = Key::Other(key.code);
-                enigo.key(key, enigo::Direction::Press)?;
-            }
-
-            for key in keys.keys {
-                let key = Key::Other(key.code);
-                enigo.key(key, enigo::Direction::Click)?;
-            }
-
-            for key in keys.modifiers {
-                let key = Key::Other(key.code);
-                enigo.key(key, enigo::Direction::Release)?;
-            }
-        }
-        "clipboard" => {
-            let data: ClipboardProperties =
-                serde_json::from_value(serde_json::Value::Object(properties))?;
-            if let Some(text) = data.text {
-                let mut clipboard = Clipboard::new()?;
-                clipboard.set_text(text)?;
-            }
-        }
-        action_id => {
-            tracing::warn!(?action_id, ?context, "unknown internal action");
-        }
-    }
-
-    Ok(())
+        Ok::<(), anyhow::Error>(())
+    });
 }
