be2eea317b49b325ff79b8f12c701fcd48db577b
Jacobtread <jacobtread@gmail.com>
2025-04-28 21:20:03 +1200

    feat: use nick_name for device name on desktop

--- src-tauri/src/utils/device.rs
+++ src-tauri/src/utils/device.rs
@@ -1,3 +1,17 @@
 pub fn get_device_name() -> String {
-    gethostname::gethostname().to_string_lossy().to_string()
+    let name = match nick_name::NickName::new() {
+        Ok(value) => value,
+        Err(cause) => {
+            tracing::error!(?cause, "failed to get device nickname name");
+            return "Unknown".to_string();
+        }
+    };
+
+    match name.get() {
+        Ok(value) => value,
+        Err(cause) => {
+            tracing::error!(?cause, "failed to get device name");
+            "Unknown".to_string()
+        }
+    }
 }
