8caf195782de1f9cfa358e4c8bf3391bf866565c
Jacobtread <jacobtread@gmail.com>
2025-04-29 00:41:40 +1200

    fix: error type for http result

--- src-tauri/src/server/routes/server.rs
+++ src-tauri/src/server/routes/server.rs
@@ -1,23 +1,40 @@
-use axum::{Extension, Json};
+use axum::{Extension, Json, http::StatusCode};
+use thiserror::Error;
 
 use crate::{
-    database::{DbPool, entity::settings::SettingsModel},
-    server::models::{error::HttpResult, server::ServerDetails},
-    utils::device::get_device_name,
+    database::{DbErr, DbPool, entity::settings::SettingsModel},
+    server::models::{
+        error::{HttpError, HttpResult},
+        server::ServerDetails,
+    },
 };
 
 const IDENTIFIER: &str = "TILEPAD_CONTROLLER_SERVER";
+
+#[derive(Debug, Error)]
+pub enum ServerError {
+    #[error("failed to load settings")]
+    GetSettings(#[from] DbErr),
+}
 
 /// GET /server/details
 ///
 /// Get simple details about the server, used to check if a server
 /// is alive by device clients
 pub async fn details(Extension(db): Extension<DbPool>) -> HttpResult<ServerDetails> {
-    let settings = SettingsModel::get_or_default(&db).await?;
+    let settings = SettingsModel::get_or_default(&db)
+        .await
+        .map_err(ServerError::GetSettings)?;
     let name = settings.config.device_name;
 
     Ok(Json(ServerDetails {
         identifier: IDENTIFIER,
         hostname: name,
     }))
+}
+
+impl HttpError for ServerError {
+    fn status(&self) -> StatusCode {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
 }
