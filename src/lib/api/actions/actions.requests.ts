ad2110e1cb5ee0d372c564314bda44b864f97ce4
Jacobtread <jacobtread@gmail.com>
2025-07-22 01:51:02 +1200

    fix: action category type import

--- src/lib/api/actions/actions.requests.ts
+++ src/lib/api/actions/actions.requests.ts
@@ -1,9 +1,11 @@
-import type ActionCategory from "$lib/components/actions/ActionCategory.svelte";
-
 import { invoke } from "@tauri-apps/api/core";
 
 import type { PluginId } from "../types/plugin";
-import type { ActionId, ActionWithCategory } from "../types/actions";
+import type {
+  ActionId,
+  ActionCategory,
+  ActionWithCategory,
+} from "../types/actions";
 
 export function getActions() {
   return invoke<ActionCategory[]>("actions_get_actions");
