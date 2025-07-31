c7711c1de3726098a759e94a3b46878c75055cf7
Jacobtread <jacobtread@gmail.com>
2025-08-01 01:32:31 +1200

    refactor: replace TestTileContainer with template render snippet

--- src/lib/components/tiles/TileLabel.stories.svelte
+++ src/lib/components/tiles/TileLabel.stories.svelte
@@ -1,4 +1,6 @@
 <script lang="ts" module>
+  import type { ComponentProps } from "svelte";
+
   import { defineMeta } from "@storybook/addon-svelte-csf";
   import {
     LabelAlign,
@@ -6,14 +8,13 @@
   } from "$lib/api/types/tiles";
 
   import TileLabel from "./TileLabel.svelte";
-  import TestTileContainer from "./TestTileContainer.svelte";
 
   const { Story } = defineMeta({
     title: "Tiles/TileLabel",
     component: TileLabel,
     tags: ["autodocs"],
     argTypes: {},
-    decorators: [() => TestTileContainer as never],
+    render: template,
   });
 
   const defaultLabel: TileLabelType = {
@@ -30,6 +31,38 @@
     outline_color: "#000000",
   };
 </script>
+
+{#snippet template(args: ComponentProps<typeof TileLabel>)}
+  <button
+    style="--tile-size-adjustment: 1; --tile-width: 128px; --tile-height: 128px;"
+    class="tile"
+    aria-roledescription="button"
+    data-drop-zone="filledTile"
+  >
+    <TileLabel {...args} />
+  </button>
+
+  <style>
+    .tile {
+      position: relative;
+      border-radius: 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: var(--tile-width);
+      height: var(--tile-height);
+      color: #ccc;
+
+      border: 2px solid var(--tile-border-color);
+      cursor: pointer;
+
+      background-color: #151318;
+
+      user-select: none;
+      overflow: hidden;
+    }
+  </style>
+{/snippet}
 
 <Story
   name="Default"
