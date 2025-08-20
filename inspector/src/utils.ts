c5cd834b0f04270a61ae40e4b4f1ab62d0eb1153
Jacobtread <jacobtread@gmail.com>
2025-08-20 20:12:25 +1200

    fix: properly type debounce from inspector and display

--- inspector/src/utils.ts
+++ inspector/src/utils.ts
@@ -1,24 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 /**
  * Helper to debounce calls to a function to ensure that
  * a delay has elapsed between calls
- * 
- * @param {*} fn The function to call
- * @param {*} delay The delay to wait before calling (Reset if called before the delay has elapsed)
+ *
+ * @param fn The function to call
+ * @param delay The delay to wait before calling (Reset if called before the delay has elapsed)
  * @returns The debounced function
  */
-export function debounce(fn, delay) {
-    let timeoutId;
+export function debounce<T extends (...args: any[]) => any>(
+  fn: T,
+  delay: number,
+) {
+  let timeoutId: ReturnType<typeof setTimeout> | undefined;
 
-    return function (...args) {
-        // Clear the previous timeout
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
+  return function (...args: Parameters<T>) {
+    // Clear the previous timeout
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
 
-        // Set a new timeout with the specified delay
-        timeoutId = setTimeout(() => {
-            fn.apply(this, args);
-        }, delay);
-    };
+    // Set a new timeout with the specified delay
+    timeoutId = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
 }
