1cd8e4f3baa51f30d0cb4501ced57d45edf7f7b4
Jacobtread <jacobtread@gmail.com>
2025-08-29 21:15:09 +1200

    feat: update and improve prettier and eslint configs
    Make them compatable with Zed

--- src/lib/api/plugins/plugins.events.ts
+++ src/lib/api/plugins/plugins.events.ts
@@ -5,17 +5,17 @@ import type { PluginId } from "../types/plugin";
 import { queryClient } from "../client";
 import { invalidatePluginsQuery } from "./plugins.mutators";
 
-listen<PluginId>("plugins:loaded", ({ payload: plugin_id }) => {
+listen<PluginId>("plugins:loaded", ({ payload: _ }) => {
   invalidatePluginsQuery(queryClient);
 });
 
-listen<PluginId>("plugins:unloaded", ({ payload: plugin_id }) => {
+listen<PluginId>("plugins:unloaded", ({ payload: _ }) => {
   invalidatePluginsQuery(queryClient);
 });
 
 listen<{ plugin_id: PluginId; state: string }>(
   "plugins:task_state_changed",
-  ({ payload: { plugin_id, state } }) => {
+  ({ payload: _ }) => {
     invalidatePluginsQuery(queryClient);
   },
 );
