456358ac382566d9ebeb20c95cb52acee241b5ca
Jacobtread <jacobtread@gmail.com>
2026-02-08 13:07:14 +1300

    fix: storybook svelte addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,8 @@
 import type { StorybookConfig } from "@storybook/sveltekit";
 
-import { join, dirname } from "path";
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
-}
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
-  addons: [getAbsolutePath("@storybook/addon-svelte-csf")],
+  addons: ["@storybook/addon-svelte-csf"],
   framework: "@storybook/sveltekit",
   // framework: {
   //   name: getAbsolutePath("@storybook/sveltekit"),
